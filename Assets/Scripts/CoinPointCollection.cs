7a9ecb6a04358f017c502c0e3e7d1408d71d5ff0
Weebtrain <156955415+Weebtrain@users.noreply.github.com>
2025-03-01 12:50:40 -0800

    Score System
    Coins now are removed upon collection, and update the score.

--- Assets/Scripts/CoinPointCollection.cs
+++ Assets/Scripts/CoinPointCollection.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinPointCollection : MonoBehaviour
 {
+    public UnityEvent CoinCollection = new UnityEvent();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,13 @@ public class CoinPointCollection : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter (Collider triggeredObject)
+    {
+        if (triggeredObject.CompareTag("Player")) {
+            CoinCollection?.Invoke();
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,31 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CoinPointCollection[] coins = FindObjectsByType<CoinPointCollection>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach (CoinPointCollection c in coins)
+        {
+            c.CoinCollection.AddListener(UpdateScore);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void UpdateScore ()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
     }
 }
